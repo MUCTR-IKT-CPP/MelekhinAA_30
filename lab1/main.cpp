dbdf9d881efa79ba09d489ea086d4b9e01167dd4
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-09-04 17:16:06 +0300

    Лаба 1 сделана

---
+++ lab1/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+
+using namespace std;
+
+/*
+ * Генерация случайной строки
+ *
+ * @param lenth длина строки.
+ * @return возвращает сгенерированноую строку.
+ */
+string makeString(int lenth){
+    string str = "";
+    srand(time(0));
+    for(int i = 0; i < lenth; i++){
+        str += 'a' + rand() % 26;
+    }
+    return str;
+}
+
+
+/*
+ * Подчёт символов в строке и вывод количства символов
+ *
+ * @param string строка.
+ * @param lenth длина исследуемой строки.
+ */
+void countRandomChars(string str, int lenth){
+    map<char, int> charsCount;
+    for(int i = 0; i < lenth; i++){
+        if(charsCount.find(str[i]) != charsCount.end()){
+            charsCount[str[i]] += 1;
+        }else{
+            charsCount[str[i]] = 1;
+        }
+    }
+    cout << "Случайная строка: " << str << endl;
+    for(const auto& pair : charsCount){
+        std::cout << "символ " << pair.first << ": " << pair.second << std::endl;
+    }
+}
+
+
+int main(){
+    const int LENTH = 10000;
+    string str = makeString(LENTH);
+    countRandomChars(str, LENTH);
+    return 0;
+}
