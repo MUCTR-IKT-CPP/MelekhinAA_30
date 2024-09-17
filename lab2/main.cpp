0fadd38e6840222a7b938804bc1c39fdc8dabbf1
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-09-17 16:45:32 +0300

    Лаба 2 сдана

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -125,11 +125,7 @@ void pascalArray(int** array, int N) {
     for (int i = 0; i < N; i++) {
         array[i][0] = 1;  
         for (int j = 1; j <= i; j++) {
-            if (j == i) {
-                array[i][j] = 1;  
-            } else {
-                array[i][j] = array[i-1][j-1] + array[i-1][j];
-            }
+            array[i][j] = array[i-1][j-1] + array[i-1][j];
         }
     }
 }
